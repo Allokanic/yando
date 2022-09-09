74442d8818a99e36e782b426aeca1a1db4b09f09
Allokanic <neon.eagle@mail.ru>
2022-09-09 10:00:17 +0300

    Initial commit

---
+++ src/main/java/dev/allokanic/yando/YandoApplication.java
@@ -0,0 +1,13 @@
+package dev.allokanic.yando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandoApplication.class, args);
+	}
+
+}
